21ceecea342523d43e2c3ed7260cc10bbb568f77
ICE0208 <doky3307@naver.com>
2024-05-18 11:11:59 +0900

    feat: 일기 작성 페이지의 헤더 추가

--- screens/New/index.tsx
+++ screens/New/index.tsx
@@ -1,5 +1,21 @@
-import { View } from "react-native";
+import ModalHeader from "components/ModalHeader";
+import { Text, View } from "react-native";
 
 export default function NewScreen() {
-  return <View></View>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "rgba(0,0,0,0.85)",
+        alignItems: "center",
+        padding: 20,
+      }}
+    >
+      <ModalHeader
+        left={{ text: "취소", onPress: (e) => {} }}
+        right={{ text: "확인", onPress: (e) => {} }}
+        center={{ text: "새로운 일기" }}
+      />
+    </View>
+  );
 }

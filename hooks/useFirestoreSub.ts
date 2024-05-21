a9ced8a6fb860b25b66a7c2f627df1670ca65a6d
ICE0208 <doky3307@naver.com>
2024-05-22 05:18:56 +0900

    refactor: hooks 확장자 통일

--- hooks/useFirestoreSub.ts
+++ hooks/useFirestoreSub.ts
@@ -1,5 +1,4 @@
 import { useContext } from "react";
 import { FirestoreSubContext } from "components/FirestoreSubProvider";
 
-// Hook to use the context
 export const useFirestoreSub = () => useContext(FirestoreSubContext);
